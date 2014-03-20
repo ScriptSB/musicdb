be9d2cb4542ea97dd229f8a8045d8c567c472900
Ruofan Zhou <zrfanzy@gmail.com>
2014-03-20 11:07:02 +0100

    .

---
+++ models/ddl.sql
@@ -0,0 +1,93 @@
+--ENTITY area
+CREATE TABLE  area (
+  AID INTEGER,
+  Aname CHAR(45),
+  Atype CHAR(45),
+  PRIMARY KEY (AID));
+  
+--ENTITY genre
+--we don't need gcount in genre
+CREATE TABLE  genre (
+  GID INTEGER,
+  Gname CHAR(45),
+  PRIMARY KEY (GID));
+
+--ENTITY artist
+CREATE TABLE  artist (
+  arID INTEGER,
+  name CHAR(45) NULL,
+  type CHAR(45) NULL,
+  gender CHAR(45) NULL,
+  PRIMARY KEY (arID));
+
+--ENTITY track
+CREATE TABLE  track (
+  TID INTEGER,
+  position INTEGER,
+  recording_RID INT NOT NULL,
+  artist_ID INT NOT NULL,
+  artist_area_AID INT NOT NULL,
+  artist_genre_GID INT NOT NULL,
+  media_MID INT NOT NULL,
+  media_release_REID INT NOT NULL,
+  PRIMARY KEY (TID));
+  
+--ENTITY recording
+CREATE TABLE  recording (
+  RID INTEGER,
+  Rname CHAR(45),
+  Rlength CHAR(45),
+  PRIMARY KEY (RID));
+
+--ENTITY release
+CREATE TABLE  release (
+  REID INTEGER,
+  Relname CHAR(45),
+  PRIMARY KEY (REID));
+
+--ENTITY media
+CREATE TABLE  media (
+  MID INTEGER,
+  Mformat CHAR(45),
+  release_REID INTEGER,
+  PRIMARY KEY (MID));
+    
+--RELATIONSHIP artist-area
+CREATE TABLE artist_area (
+  arID INTEGER NOT NULL,
+  AID INTEGER,
+  PRIMARY KEY (arID, AID),
+  FOREIGN KEY (arID) REFERENCES artist,
+  FOREIGN KEY (AID) REFERENCES area);
+
+--RELATIONSHIP artist_genre
+CREATE TABLE artist_genre (
+  arID INTEGER NOT NULL,
+  GID INTEGER NOT NULL,
+  PRIMARY KEY (arID, GID),
+  FOREIGN KEY (arID) REFERENCES artist,
+  FOREIGN KEY (GID) REFERENCES genre);
+    
+--RELATIONSHIP media_recording
+CREATE TABLE media_recording(
+  MID INTEGER,
+  REID INTEGER,
+  PRIMARY KEY (MID, REID),
+  FOREIGN KEY (MID) REFERENCES media,
+  FOREIGN KEY (REID) REFERENCES recording);
+  
+--RELATIONSHIP artist_track
+CREATE TABLE artist_track(
+  arID INTEGER NOT NULL,
+  TID INTEGER NOT NULL,
+  PRIMARY KEY (arID, TID),
+  FOREIGN KEY (arID) REFERENCES artist,
+  FOREIGN KEY (TID) REFERENCES track);
+
+--RELATIONSHIP trackR
+CREATE TABLE trackR(
+  TID INTEGER, RID INTEGER, MID INTEGER,
+  PRIMARY KEY (TID, RID, MID),
+  FOREIGN KEY (RID) REFERENCES recording,
+  FOREIGN KEY (TID) REFERENCES track,
+  FOREIGN KEY (MID) REFERENCES media);
